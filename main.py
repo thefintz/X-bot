f8c699b446716d66eb6a128c675b90aa86c6553c
Alexandre <alexandre.bueno0204@gmail.com>
2024-10-29 14:45:57 -0300

    add time to make the tweet unique

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from dotenv import load_dotenv
+from datetime import datetime # so p horario, remover depois
 import os
 import tweepy
 
@@ -9,8 +9,8 @@ access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
-
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text="Hi X!")
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # horario so p deixar o tweet unico pois n pode ser igual 2x em curto periodo
+client.create_tweet(text=f"Hi X! Time: {current_time}")
