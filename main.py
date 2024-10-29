bc32f838459cc67962fd32a982f03a448a621697
Alexandre <alexandre.bueno0204@gmail.com>
2024-10-29 14:22:25 -0300

    .

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_t
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text="Hello X!")
+client.create_tweet(text="Hi X!")
