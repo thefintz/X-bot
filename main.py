9814fd38720e600f3c2b2189946e1711776bedc0
Alexandre <alexandre.bueno0204@gmail.com>
2024-10-29 14:17:40 -0300

    first time with github actions

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+import tweepy
+
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+bearer_token = os.getenv("BEARER_TOKEN")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+client.create_tweet(text="Hello X!")
