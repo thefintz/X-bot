aa650b74a4905373ce99d0e71dbc9b015d845171
Alexandre <alexandre.bueno0204@gmail.com>
2024-10-31 12:32:38 -0300

    should work version

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def fetch_links():
     with open("response.json", "w") as outfile:
         json.dump(response.json(), outfile, ensure_ascii=False, indent=4)
     
-    # Extrai links do arquivo view_links.json
+    # pega os links do arquivo view_links.json
     with open("response.json", "r") as file:
         data = json.load(file)
         dados = data["d"]["dados"]
@@ -46,18 +46,20 @@ def fetch_links():
     
     # Carregar links antigos
     try:
-        with open("view_links.json", "r") as file:
-            old_links = json.load(file)
+        with open("last_posted.json", "r") as file:
+            last_posted = json.load(file)
     except FileNotFoundError:
-        old_links = []
+        last_posted = []
 
     # Adicionar apenas novos links e atualizar o arquivo view_links.json
-    new_links = [link for link in view_links if link not in old_links]
+    new_links = [link for link in view_links if link not in last_posted]
     if new_links: # verificar se nao esta vazio
-        view_links = old_links + new_links  # Atualiza view_links com todos os links ja encontrados
+        view_links = last_posted + new_links  # Atualiza view_links com todos os links ja encontrados
         with open("view_links.json", "w") as outfile: # abrimos view_links.json e gravamos view_links dentro dele usando json.dump
             json.dump(view_links, outfile, indent=4, ensure_ascii=False)
-        print(f"{len(new_links)} novos links adicionados.")
+        protocolo_ids = [f"id: {re.search(r'NumeroProtocoloEntrega=(\d+)', link).group(1)}" for link in new_links]
+        print(f"{len(new_links)} novo(s) link(s)encontrado(s) e adicionado(s): {', '.join(protocolo_ids)}")
+        print("\n")
     else:
         print("Nenhum link novo encontrado.")
 
@@ -76,34 +78,33 @@ def post_tweets(new_links):
     try:
         with open("last_posted.json", "r") as file:
             posted_links = json.load(file)
-        print("Historico de links postados carregado:", posted_links)
     except FileNotFoundError:
         posted_links = []
-        print("Arquivo last_posted.json nao encontrado; inicializando como lista vazia.")
 
-    # Se nao houver novos links para postar, termina a funcao sem erro
     if not new_links:
         print("Nao ha novos links para serem postados.")
         return
 
-    # Posta apenas os links novos
+    # se tem links novos, entra no for e posta apenas esses links novos
     for link_to_post in new_links:
         print(f"Postando tweet: {link_to_post}")
         try:
             client.create_tweet(text=f"Link do documento: {link_to_post}")
             print(f"Tweet postado: {link_to_post}")
         except tweepy.errors.Forbidden:
-            print(f"Tweet duplicado detectado e ignorado: {link_to_post}")
+            print(f"Tweet duplicado detectado e ignorado ou erro: {link_to_post}")
             continue
 
         # Atualiza o historico com o novo link postado
+        tamanho_antes = len(posted_links)
         posted_links.append(link_to_post)
         with open("last_posted.json", "w") as file:
             json.dump(posted_links, file, indent=4, ensure_ascii=False)
-        print("Historico atualizado:", posted_links)
+        print(f"Quantidade de links postados ao total: antes = {tamanho_antes}, agora = {len(posted_links)}")
+        print("\n")
 
-        time.sleep(30)
 
-# Executa as funcoes
+        time.sleep(60)
+
 new_links = fetch_links()
 post_tweets(new_links)
